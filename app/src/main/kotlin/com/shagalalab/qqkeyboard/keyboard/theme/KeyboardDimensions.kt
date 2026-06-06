097cd3944f7c8dde43f2cfb9a27525a94aa0d672
Atabek <atabekmur@gmail.com>
2026-06-06 17:17:00 +1000

    Increase KeyboardDimensions' rowGap

--- app/src/main/kotlin/com/shagalalab/qqkeyboard/keyboard/theme/KeyboardDimensions.kt
+++ app/src/main/kotlin/com/shagalalab/qqkeyboard/keyboard/theme/KeyboardDimensions.kt
@@ -3,7 +3,7 @@ package com.shagalalab.qqkeyboard.keyboard.theme
 import androidx.compose.ui.unit.dp
 
 object KeyboardDimensions {
-    val rowGap = 4.dp               // spacedBy in column
+    val rowGap = 8.dp               // spacedBy in column
     val gridPadding = 2.dp          // padding around key grid (Box + Column)
     val keyHorizontalPadding = 2.dp // visual gap between keys
     val suggestionStripHeight = 40.dp
