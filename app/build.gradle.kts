fdbdeef3f7154263e9bc47d1309d9124cd9e13fd
Atabek <atabekmur@gmail.com>
2026-08-02 13:07:01 +1000

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.shagalalab.qqkeyboard"
         minSdk = 26
         targetSdk = 37
-        versionCode = 28
-        versionName = "1.2"
+        versionCode = 29
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
